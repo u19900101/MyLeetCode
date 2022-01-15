154f131065172a9caac4a86caae0eee45606c9b4
u19900101 <815000342@qq.com>
2022-01-15 10:55:30 +0800

    算法： 二分查找: 35搜索插入位置

---
+++ log-demo/src/main/java/zuo/leetcode/_035_搜索插入位置.java
@@ -0,0 +1,73 @@
+package zuo.leetcode;
+
+/**
+ * @author pppppp
+ * @date 2022/1/15 9:41
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * 示例 1:
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ * <p>
+ * 示例 3:
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ * <p>
+ * 示例 4:
+ * 输入: nums = [1,3,5,6], target = 0
+ * 输出: 0
+ * <p>
+ * 示例 5:
+ * 输入: nums = [1], target = 0
+ * 输出: 0
+ */
+public class _035_搜索插入位置 {
+    public static void main(String[] args) {
+        // int[] nums = {1, 3, 5, 6};
+        // int[] target = {5, 2, 7, 0, 0};
+        // int[] expected = {2, 1, 4, 0, 0};
+
+        int[] nums = {1, 2};
+        int[] target = {0,3};
+        int[] expected = {0,2};
+
+        for (int i = 0; i < target.length; i++) {
+            int res = searchInsert(nums, target[i]);
+            System.out.println(res == expected[i]);
+        }
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        /*判断目标值在开头还是结尾*/
+       /* if (target == nums[0] || target == nums[nums.length - 1]) {
+            return target == nums[0] ? 0 : nums.length - 1;
+        }
+
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return target < nums[0] ? 0 : nums.length;
+        }*/
+
+        int L = 0;
+        int R = nums.length - 1;
+        while (L <= R) {
+            int mid = L + ((R - L) >> 1);
+            if (nums[mid] < target) {
+                L = mid + 1;
+            } else if (nums[mid] > target) {
+                R = mid - 1; //当R = L + 1时，此时的操作将使R = L-1
+            } else {
+                return mid;
+            }
+        }
+        /*在数组中不存在该数，返回应该插入的位置*/
+        // return target > nums[L] ? L + 1 : L;
+        return R + 1;
+    }
+}
--- log-demo/src/main/java/zuo/leetcode/_704_二分查找.java
+++ log-demo/src/main/java/zuo/leetcode/_704_二分查找.java
@@ -9,73 +9,111 @@ package zuo.leetcode;
 public class _704_二分查找 {
 
     public static void main(String[] args) {
-        // int[] nums = {-1, 0, 3, 5, 9, 12};
-        int[] nums = {2,5};
-        int target = 7;
-        // int target = 9;
-        int search = search(nums, target);
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        // int[] nums = {2, 5};
+        // int target = 7;
+        int target = 122;
+        int search = search4(nums, target);
         System.out.println(search);
     }
 
-    public static int search(int[] nums, int target) {
-        if(nums[0] == target){
+    public static int search4(int[] nums, int target) {
+        if (nums[0] == target) {
             return 0;
-        }else if(nums[nums.length-1] == target){
-            return nums.length-1;
+        } else if (nums[nums.length - 1] == target) {
+            return nums.length - 1;
         }
         int L = 0;
-        int R = nums.length-1;
-        while (L < R){
-            int mid = L + ((R-L)>> 1);
-            if(nums[mid] == target){
+        int R = nums.length;
+        while (L < R) {
+            int mid = L + ((R - L) >> 1);
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid] > target){
-                R = mid-1;
-            }else {
-                L = mid+1;//当L + 1 = R 时，L退化为R
+            } else if (nums[mid] > target) {
+                R = mid;
+            } else {
+                L = mid + 1;//当L + 1 = R 时，L退化为R
+            }
+        }
+        return -1;
+    }
+    public static int search3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1; // 定义target在左闭右闭的区间里，[left, right]
+        while (left <= right) { // 当left==right，区间[left, right]依然有效，所以用 <=
+            int middle = left + ((right - left) >> 1);// 防止溢出 等同于(left + right)/2
+            if (nums[middle] > target) {
+                right = middle - 1; // target 在左区间，所以[left, middle - 1]
+            } else if (nums[middle] < target) {
+                left = middle + 1; // target 在右区间，所以[middle + 1, right]
+            } else { // nums[middle] == target
+                return middle; // 数组中找到目标值，直接返回下标
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static int search(int[] nums, int target) {
+        if (nums[0] == target) {
+            return 0;
+        } else if (nums[nums.length - 1] == target) {
+            return nums.length - 1;
+        }
+        int L = 0;
+        int R = nums.length - 1;
+        while (L < R) {
+            int mid = L + ((R - L) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;//当L + 1 = R 时，L退化为R
             }
         }
         // if(nums[R] == target){
         //     return R;
         // }
 
-        return nums[L] == target ? L :-1;
+        return nums[L] == target ? L : -1;
     }
 
     public static int search0(int[] nums, int target) {
-        if(nums[0] == target){
+        if (nums[0] == target) {
             return 0;
-        }else if(nums[nums.length-1] == target){
-            return nums.length-1;
+        } else if (nums[nums.length - 1] == target) {
+            return nums.length - 1;
         }
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
 
-        return getIndex(start,end,nums,target);
+        return getIndex(start, end, nums, target);
 
     }
 
-    private static int getIndex(int start, int end, int[] nums,int target) {
-        int mid = start + ((end-start)>> 1);
+    private static int getIndex(int start, int end, int[] nums, int target) {
+        int mid = start + ((end - start) >> 1);
         /*到达边界 停止*/
-        if(mid == 0 || mid == nums.length-1){
-            if(nums[start] == target){
+        if (mid == 0 || mid == nums.length - 1) {
+            if (nums[start] == target) {
                 return start;
             }
 
-            if(nums[end] == target){
+            if (nums[end] == target) {
                 return end;
             }
             return -1;
-        }else {
+        } else {
             if (nums[mid] > target) {
-                end = mid-1;
+                end = mid - 1;
             } else if (nums[mid] < target) {
                 start = mid + 1;
             } else {
                 return mid;
             }
         }
-        return getIndex(start, end, nums,target);
+        return getIndex(start, end, nums, target);
     }
 }
