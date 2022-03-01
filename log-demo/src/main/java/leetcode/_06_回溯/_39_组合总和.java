4c475130974964c0e522c1b5ab91996e0eaecb4b
u19900101 <815000342@qq.com>
2022-03-01 15:07:04 +0800

    回溯 39_组合总和 -- 画图  + 不重复回溯 + 剪枝

---
+++ log-demo/src/main/java/leetcode/_06_回溯/_39_组合总和.java
@@ -0,0 +1,75 @@
+package leetcode._06_回溯;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author pppppp
+ * @date 2022/3/1 13:42
+ * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，
+ * 找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。
+ * 你可以按 任意顺序 返回这些组合。
+ * <p>
+ * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+ * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+ * <p>
+ * 示例 1：
+ * 输入：candidates = [2,3,6,7], target = 7
+ * 输出：[[2,2,3],[7]]
+ * 解释：
+ * 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+ * 7 也是一个候选， 7 = 7 。
+ * 仅有这两种组合。
+ * <p>
+ * 示例 2：
+ * 输入: candidates = [2,3,5], target = 8
+ * 输出: [[2,2,2,2],[2,3,3],[3,5]]
+ * <p>
+ * 示例 3：
+ * 输入: candidates = [2], target = 1
+ * 输出: []
+ */
+public class _39_组合总和 {
+
+    @Test
+    public void T_() {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        List<List<Integer>> lists = combinationSum(candidates, target);
+        System.out.println();
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        dfs(candidates, target, 0);
+        return res;
+    }
+
+    public void dfs(int[] candidates, int target, int curIndex) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if (target < 0 || curIndex == candidates.length) {
+            return;
+        }
+
+        for (int i = curIndex; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dfs(candidates, target - candidates[i], i);
+            path.removeLast();
+            /*剪枝操作*/
+            if(target - candidates[i] < 0){
+                break;
+            }
+        }
+    }
+
+}
